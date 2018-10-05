d014390cb54341465156c563dda94be23099d2f2
Han Zhang <eveline1129@163.com>
2018-10-05 16:55:32 -0500

    styled header a little bit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,51 @@ import logo from '../images/Alto_logo.png';
 
 const Header = () => (
     <header>
-        <div>
-            <img src={logo} alt='alto logo' />
+        <div className="container">
+            <img src={logo} alt='alto logo' className="header__img" />
             {/*A special version of the <Link> that will add styling attributes to the rendered element when it matches the current URL.Adde is-active class to sass style file*/}
         </div>
-        <NavLink exact to="/" activeClassName="is-active"> Trip</NavLink>
-        <NavLink to="/driver" activeClassName="is-active"> Driver</NavLink>
-        <NavLink to="/auto" activeClassName="is-active"> Auto</NavLink>
-        <NavLink to="/route" activeClassName="is-active"> Route</NavLink>
+        <div className="container">
+            <div className="nav__item">
+                <NavLink
+                    exact to="/"
+                    activeClassName="is-active"
+                    className="Nav_link"
+                >
+                    Trip
+                </NavLink>
+            </div>
+
+            <div className="nav__item">
+                <NavLink
+                    to="/driver"
+                    activeClassName="is-active"
+                    className="Nav_link"
+                >
+                    Driver
+                </NavLink>
+            </div>
+
+            <div className="nav__item">
+                <NavLink
+                    to="/auto"
+                    activeClassName="is-active"
+                    className="Nav_link"
+                >
+                    Auto
+                </NavLink>
+            </div>
+
+            <div className="nav__item">
+                <NavLink
+                    to="/route"
+                    activeClassName="is-active"
+                    className="Nav_link"
+                >
+                    Route
+                </NavLink>
+            </div>
+        </div>
     </header>
 );
 
