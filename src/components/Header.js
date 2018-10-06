3c886ed87e5c43f77dcfcf1ba030117e49522124
Han Zhang <eveline1129@163.com>
2018-10-06 01:01:36 -0500

    added profile and vibes icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../images/Alto_logo.png';
+import profile from '../images/Profile_icon.png';
+import vibes from '../images/Vibes_icon.png';
+
 // https://reacttraining.com/react-router/web/api/NavLink
 
 const Header = () => (
     <header>
-        <div className="container container--centered container--center">
+        <div className="container container--spacebetween container--center">
+            <img src={profile} alt='profile' className="header__img" />
+
             <img src={logo} alt='alto logo' className="header__img" />
             {/*A special version of the <Link> that will add styling attributes to the rendered element when it matches the current URL.Adde is-active class to sass style file*/}
+            <img src={vibes} alt='vibes' className="header__img" />
+
         </div>
         <div className="container container--centered container--center">
             <div className="nav__item">
