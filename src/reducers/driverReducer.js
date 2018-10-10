082607e46aa97fbf43d43dfaba849af43bcc4eee
Han Zhang <eveline1129@163.com>
2018-10-10 06:05:46 -0500

    minor change

--- src/reducers/driverReducer.js
+++ src/reducers/driverReducer.js
@@ -10,12 +10,12 @@ const driverReducer = (state = initialState, action) => {
         case 'LOAD_DRIVER_REQUEST':
             // return state.set('fetching', true)
             //     .set('error', null);
-            return { ...state, driver_fetchingfetching: true }
+            return { ...state, driver_fetching: true }
         case 'LOAD_DRIVER_SUCCESS':
             // return state.set('fetching', false)
             //     .set('data', action.data)
             //     .set('error', null);
-            return { ...state, fetching: false, driver: action.data }
+            return { ...state, driver_fetching: false, driver: action.data }
         case 'LOAD_DRIVER_FAILURE':
             // return state.set('fetching', false)
             //     .set('data', null)
