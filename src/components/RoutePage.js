9fa942baf5d9c81d7fc93a1a637bed9c85cb4ce1
Han Zhang <eveline1129@163.com>
2018-10-11 14:41:52 -0500

    fixed map_icon not responsive

--- src/components/RoutePage.js
+++ src/components/RoutePage.js
@@ -29,7 +29,7 @@ class RoutePage extends React.Component {
         return (
             <div className="container container--centered container--column">
                 <div className="container-withoutPadding container--centered">
-                    <div className="route__image route__image--align">
+                    <div className=" route__image route__image--align">
 
                         <img
                             src={map}
@@ -39,37 +39,40 @@ class RoutePage extends React.Component {
                                 height: 'auto',
                             }}
                         />
-                    </div>
-                    <img src={mapIcon} alt="current location icon" style={{ position: 'absolute', right: 20 }} />
-                </div>
-                <div className="container">
-                    <p className="subtitle">YOUR TRIP</p>
-                </div>
-                <div className="container alignItems">
-                    <p style={{ fontSize: 48, margin: 0, fontFamily: 'PxGroteskLight' }}>
-                        {time[0]}
-                    </p>
-                    <p style={{ fontSize: 30, margin: 0, paddingLeft: 10, paddingBottom: 5, fontFamily: 'PxGroteskLight' }}>
-                        {time[1]}
-                    </p>
-                </div>
-                <div className="container container--flexstart ">
-                    <p style={{ marginTop: 0, fontFamily: 'PxGroteskLight', fontSize: 13, color: 'rgb(63,56,37)' }}>
-                        {dropoff_name}
-                    </p>
-                </div>
-                <div className="container container--column topBorder">
-                    <div style={{ marginTop: 10, fontSize: 12, color: 'rgb(108,104,91)', fontFamily: 'PxGroteskLight', }}>Current Vibe</div>
-                    <div style={{ marginTop: 6, fontSize: 12, color: 'rgb(108,104,91)', fontFamily: 'PxGroteskRegular', }}>{vibe.name}</div>
 
-                </div>
-                <div className="container--withoutPadding marginTop">
-                    <button className="route-big-button">
-                        CHANGE VEHICLE VIBE
+                        <div className="container ">
+                            <img src={mapIcon} alt="current location icon" className="route__image--alignself" />
+
+                        </div>
+                    </div>
+                    <div className="container">
+                        <p className="subtitle">YOUR TRIP</p>
+                    </div>
+                    <div className="container alignItems">
+                        <p style={{ fontSize: 48, margin: 0, fontFamily: 'PxGroteskLight' }}>
+                            {time[0]}
+                        </p>
+                        <p style={{ fontSize: 30, margin: 0, paddingLeft: 10, paddingBottom: 5, fontFamily: 'PxGroteskLight' }}>
+                            {time[1]}
+                        </p>
+                    </div>
+                    <div className="container container--flexstart ">
+                        <p style={{ marginTop: 0, fontFamily: 'PxGroteskLight', fontSize: 13, color: 'rgb(63,56,37)' }}>
+                            {dropoff_name}
+                        </p>
+                    </div>
+                    <div className="container container--column topBorder">
+                        <div style={{ marginTop: 10, fontSize: 12, color: 'rgb(108,104,91)', fontFamily: 'PxGroteskLight', }}>Current Vibe</div>
+                        <div style={{ marginTop: 6, fontSize: 12, color: 'rgb(108,104,91)', fontFamily: 'PxGroteskRegular', }}>{vibe.name}</div>
+
+                    </div>
+                    <div className="container--withoutPadding marginTop">
+                        <button className="route-big-button">
+                            CHANGE VEHICLE VIBE
                     </button>
+                    </div>
                 </div>
             </div>
-
         );
     }
 }
