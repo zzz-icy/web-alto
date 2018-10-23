4707e870cf191ec22ddf9fceb2f87fda9a1d23c4
Han Zhang <eveline1129@163.com>
2018-10-23 16:15:52 -0500

    delete duplicate container on RoutePage'

--- src/components/RoutePage.js
+++ src/components/RoutePage.js
@@ -27,50 +27,48 @@ class RoutePage extends React.Component {
         }
         if (!vibe || !trip) return null;
         return (
-            <div className="container container--centered container--column">
-                <div className="container-withoutPadding container--centered">
-                    <div className=" route__image route__image--align">
+            <div className=" container container-withoutPadding container--centered container--column" >
+                <div className=" route__image route__image--align">
 
-                        <img
-                            src={map}
-                            alt="map overview"
-                            style={{
-                                width: '100%',
-                                height: 'auto',
-                            }}
-                        />
+                    <img
+                        src={map}
+                        alt="map overview"
+                        style={{
+                            width: '100%',
+                            height: 'auto',
+                        }}
+                    />
 
-                        <div className="container ">
-                            <img src={mapIcon} alt="current location icon" className="route__image--alignself" />
-
-                        </div>
-                    </div>
-                    <div className="container">
-                        <p className="subtitle">YOUR TRIP</p>
-                    </div>
-                    <div className="container alignItems">
-                        <p style={{ fontSize: 48, margin: 0, fontFamily: 'PxGroteskLight' }}>
-                            {time[0]}
-                        </p>
-                        <p style={{ fontSize: 30, margin: 0, paddingLeft: 10, paddingBottom: 5, fontFamily: 'PxGroteskLight' }}>
-                            {time[1]}
-                        </p>
-                    </div>
-                    <div className="container container--flexstart ">
-                        <p style={{ marginTop: 0, fontFamily: 'PxGroteskLight', fontSize: 13, color: 'rgb(63,56,37)' }}>
-                            {dropoff_name}
-                        </p>
-                    </div>
-                    <div className="container container--column topBorder">
-                        <div style={{ marginTop: 10, fontSize: 12, color: 'rgb(108,104,91)', fontFamily: 'PxGroteskLight', }}>Current Vibe</div>
-                        <div style={{ marginTop: 6, fontSize: 12, color: 'rgb(108,104,91)', fontFamily: 'PxGroteskRegular', }}>{vibe.name}</div>
+                    <div className="container ">
+                        <img src={mapIcon} alt="current location icon" className="route__image--alignself" />
 
                     </div>
-                    <div className="container--withoutPadding marginTop">
-                        <button className="route-big-button">
-                            CHANGE VEHICLE VIBE
+                </div>
+                <div className="container">
+                    <p className="subtitle">YOUR TRIP</p>
+                </div>
+                <div className="container alignItems">
+                    <p style={{ fontSize: 48, margin: 0, fontFamily: 'PxGroteskLight' }}>
+                        {time[0]}
+                    </p>
+                    <p style={{ fontSize: 30, margin: 0, paddingLeft: 10, paddingBottom: 5, fontFamily: 'PxGroteskLight' }}>
+                        {time[1]}
+                    </p>
+                </div>
+                <div className="container container--flexstart ">
+                    <p style={{ marginTop: 0, fontFamily: 'PxGroteskLight', fontSize: 13, color: 'rgb(63,56,37)' }}>
+                        {dropoff_name}
+                    </p>
+                </div>
+                <div className="container container--column topBorder">
+                    <div style={{ marginTop: 10, fontSize: 12, color: 'rgb(108,104,91)', fontFamily: 'PxGroteskLight', }}>Current Vibe</div>
+                    <div style={{ marginTop: 6, fontSize: 12, color: 'rgb(108,104,91)', fontFamily: 'PxGroteskRegular', }}>{vibe.name}</div>
+
+                </div>
+                <div className="container--withoutPadding marginTop">
+                    <button className="route-big-button">
+                        CHANGE VEHICLE VIBE
                     </button>
-                    </div>
                 </div>
             </div>
         );
